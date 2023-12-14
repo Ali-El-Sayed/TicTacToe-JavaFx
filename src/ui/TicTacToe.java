85e0b31c4da1e4d3fc7d59cefaf3edf9baa4e6e3
Ali El-Sayed <alielsayedg@gmail.com>
2023-12-14 21:20:23 +0200

    Finished SelectLevelScreen & OfflineModeScreen UI

--- src/ui/TicTacToe.java
+++ src/ui/TicTacToe.java
@@ -1,36 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ui;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import ui.Screens.SelectLevelScreen;
 
 public class TicTacToe extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/ui/Screens/selectMode.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("/ui/Screens/selectMode.fxml"));
+        Parent root = new SelectLevelScreen();
 
-        Scene scene = new Scene(root,1343,858);
+        Scene scene = new Scene(root, 1343, 858);
         stage.setResizable(false);
-        //stage.setWidth(143);
-        //stage.setHeight(858);
+
         stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
