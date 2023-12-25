acd60790c920c896853316a399ed073510895155
moahmed hussein <mh95568@gmail.com>
2023-12-25 16:38:16 +0200

    add two methods in UserDao interfacce

--- src/DataBase/UserDao.java
+++ src/DataBase/UserDao.java
@@ -5,4 +5,6 @@ import java.sql.ResultSet;
 public interface UserDao {
     void getUserData(int Id , DaoCallback<ResultSet> callback);
     void getDataForLogin(String email, DaoCallback<ResultSet> callback);
+    void addNewUser(String id,DaoCallback<Integer> callback);
+    void updateScore(int id , int isWin , DaoCallback<Integer> callback);
 }
