4b586f2be67c761e20c0f66926d75fda2b7bc53a
Ali El-Sayed <63204358+Ali-El-Sayed@users.noreply.github.com>
2023-12-26 17:28:08 +0200

    Refactor Signup Class

--- src/ui/TicTacToe.java
+++ src/ui/TicTacToe.java
@@ -1,29 +1,27 @@
 package ui;
 
-import java.awt.SplashScreen;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import ui.Screens.GameBoardScreen;
-
-import ui.Screens.SplashScreenBase;
+import ui.Screens.SignupBase;
 
 public class TicTacToe extends Application {
+
     @Override
     public void start(Stage stage) {
-        Parent root = new GameBoardScreen(stage);
+        Parent root = new SignupBase();
         Scene scene = new Scene(root, 1343, 858);
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
-        
+
     }
 
     public static void main(String[] args) throws IOException {
         launch(args);
-        
+
     }
 
 }
